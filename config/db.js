5a9e893b37e8ae2c410e8d8fd7df554fb5663dbd
0shahil0 <shahilshrestha90@gmail.com>
2024-09-22 09:27:33 +0545

    updated: db-connect file

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 import "dotenv/config"
 
 mongoose
-  .connect(process.env.DB_ADDRESS)
+  .connect(process.env.DB_ADDRESS,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+  })
   .then(() => {
     console.log("DB is connected");
   })
